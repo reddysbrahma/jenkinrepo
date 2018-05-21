ed20ebe6ba2c041f7890f10f0fea364b709e44ba
reddysbrahma <reddysbrahma@gmail.com>
2018-05-21 10:53:02 +0530

    Update check-file.py

--- check-file.py
+++ check-file.py
@@ -4,8 +4,9 @@
 
     # This program is free software: you can redistribute it and/or modify
     # it under the terms of the GNU General Public License as published by
-    # the Free Software Foundation, either version 3 of the License, or
+    #https://raymii.org/s/software/Python-script-to-monitor-a-file-for-changes-and-then-mail-the-report-with-the-file-attached.html# the Free Software Foundation, either version 3 of the License, or
     # (at your option) any later version.
+    
 
     # This program is distributed in the hope that it will be useful,
     # but WITHOUT ANY WARRANTY; without even the implied warranty of
